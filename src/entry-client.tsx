552e2bbf6a455e8dab8def61a0d4b083548e9271
Nasr Aldin <nasr2ldin@gmail.com>
2025-10-13 02:25:01 +0400

    fix: head

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -1,6 +1,8 @@
 // @refresh reload
 import { mount, StartClient } from '@solidjs/start/client';
 
+import '~/lib/hello';
+
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 mount(() => <StartClient />, document.getElementById('app')!);
 
--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -1,23 +1,17 @@
 // @refresh reload
 import { createHandler, StartServer } from '@solidjs/start/server';
 
+import AppHead from '~/components/Head';
+
 export default createHandler(() => (
   <StartServer
     document={({ assets, children, scripts }) => (
-      <html lang="en">
+      <html lang="en" dir="ltr">
         <head>
-          <meta charset="utf-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-
-          {/* Favicon links for better browser support */}
-          <link rel="icon" href="/favicon.ico" sizes="any" />
-          <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
-          <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
-          <link rel="manifest" href="/manifest.webmanifest" />
-
+          <AppHead />
           {assets}
         </head>
-        <body>
+        <body id="app-body" class="antialiased">
           <div id="app">{children}</div>
           {scripts}
         </body>
