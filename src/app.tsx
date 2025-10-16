7c5fa817752069509bd07285bbebf44f3498dc66
Nasr Aldin <nasr2ldin@gmail.com>
2025-10-16 17:18:50 +0400

    chore: update pkgs and added ErrorBoundary

--- src/app.tsx
+++ src/app.tsx
@@ -2,7 +2,7 @@ import { MetaProvider, Title } from '@solidjs/meta';
 import { Router } from '@solidjs/router';
 import { FileRoutes } from '@solidjs/start/router';
 import { QueryClient, QueryClientProvider } from '@tanstack/solid-query';
-import { Suspense } from 'solid-js';
+import { ErrorBoundary, Suspense } from 'solid-js';
 
 import ReloadPrompt from '~/components/ReloadPrompt';
 
@@ -20,7 +20,11 @@ export default function App() {
           <a href="/about">About</a>
           <a href="/test">Test</a>
           <QueryClientProvider client={queryClient}>
-            <Suspense>{props.children}</Suspense>
+            <Suspense>
+              <ErrorBoundary fallback={<div>Error by Nasr</div>}>
+                {props.children}
+              </ErrorBoundary>
+            </Suspense>
           </QueryClientProvider>
           <ReloadPrompt />
         </MetaProvider>
